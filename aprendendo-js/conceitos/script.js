4197cfaf77f593b7e8bc0fffc2a033eba1bf13d5
Julio Costa <julio.github@hotmail.com>
2022-09-28 13:13:06 -0300

    Atualizado

--- aprendendo-js/conceitos/script.js
+++ aprendendo-js/conceitos/script.js
@@ -53,6 +53,43 @@
     // Acessar valores dentro do array.
     //console.log(animais[0]);
 
+    //---------------------------------------------------
+
+    // Manipulando o Arrays
+
+    //let techs = ["html", "css", "js"]
+
+    // *adicinar um intem no fim.
+
+    //console.log(techs.push("nodejs"))
+    //console.log(techs)
+
+    // *adicionar no começo
+
+    //techs.unshift('sql')
+    //console.log(techs)
+
+    // remover item do fim.
+    //techs.pop()
+    //console.log(techs)
+
+    // remover do começo.
+    //techs.shift()
+    //console.log(techs)
+
+    // pegar somente alguns elementos do array.
+    //console.log(techs.slice(1, 3))
+    //console.log(techs)
+
+    // remover 01 ou mais itens em qualquer posição do array.
+    //techs.splice(1, 2)
+
+    // encontar a posição de um elemento no array.
+    //let index = techs.indexOf('css')
+    //console.log(index)
+
+
+
     
     
         
