5045168b2077544c2eca82a935de5c962f42b34d
Júlio Costa <julio.github@hotmail.com>
2022-09-29 22:02:22 -0300

    Novos Conceitos

--- Conceitos/script.js
+++ Conceitos/script.js
@@ -118,11 +118,36 @@
     //let decrement = 0;
     //decrement--;
     //console.log(decrement);
-    
+
     // exponencial:
     //console.log(5**5);
+//-----------------------------------------------------
+  
+    // Operadores de Comparação.
+    // irá comparar valores e retornar um boolean como resposta à comparação:
 
+    //let one = 1;
+    //let two = 2;
 
+    // == igual a.
+    //console.log( two == 1);
+    //console.log( one == '1');
+
+    // != diferente de.
+    //console.log(one != two);
+    //console.log(one != 1);
+    //console.log(one != '1');
+
+    // === estritamente igua a 
+    let one = 01;
+    let two = 02;
+
+    console.log( one === '1');
+    console.log(one === 1);
+
+    // !== estritamente diferente de
+    console.log( two !== '2');
+    console.log( two !== 2 );
 
 
     
