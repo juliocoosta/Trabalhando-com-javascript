a52bc9af92e69377680be9f9cdcabcccc011cc2a
Julio Costa <julio.github@hotmail.com>
2022-10-07 11:56:13 -0300

    Praticando

---
+++ Exercício-02/index.js
@@ -0,0 +1,31 @@
+// Transformar notas escolares:
+
+// Crie um algoritimo que transforme as notas do sistema numérico para o sistema de notas em caracteres do tipo A,B,C,D,E,F.
+
+// DE 90 PARA CIMA - A;
+// ENTRE 80 E 89 - B;
+// ENTRE 70 E 79 - C;
+// ENTRE 60 E 69 - D;
+// MENOR QUE 60  - F;
+
+var nota = 79;
+
+if(nota >= 90){
+    console.log("A")
+} else if(nota >= 80 && nota <= 89){
+    console.log('B')
+} else if(nota >=70 && nota <=79){
+    console.log('C')
+} else if(nota >=60 && nota <= 69){
+    console.log('D')
+} else {
+    console.log('F')
+}
+
+
+
+
+
+
+    
+    
