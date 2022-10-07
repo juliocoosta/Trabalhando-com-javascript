bb35f3128a037940af35446ff45f5514ca56fafb
Julio Costa <julio.github@hotmail.com>
2022-10-07 12:44:32 -0300

    Sistema de gastos familiar

---
+++ Exercício-03/index.js
@@ -0,0 +1,8 @@
+// Sistema de gastos familiar.
+
+// Crie um objeto que possuirá 02 propriedades, ambas do ripo array;
+ // receitas:[] // despesas[];
+
+ // Agora, crie uma função que irá caucular o total de receitas e despesas e irá mostrar uma mensagem se a família está com saldo positivo ou negativo, seguido do valor do saldo.
+
+ 
