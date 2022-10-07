f218ab1571a2656b406d719f3378e27f67d99ce8
Julio Costa <julio.github@hotmail.com>
2022-10-07 13:59:26 -0300

    Sistema de gastos familiar

--- Exercício-03/index.js
+++ Exercício-03/index.js
@@ -5,4 +5,33 @@
 
  // Agora, crie uma função que irá caucular o total de receitas e despesas e irá mostrar uma mensagem se a família está com saldo positivo ou negativo, seguido do valor do saldo.
 
+ const receitas = [
+    salario = 2800
+ ];
+
+ const despesas = [
+    aluguel = 400,
+    luz = 80,
+    agua = 40,
+    alimentação = 700,
+    rastreador = 80,
+    internet = 55,
+    academia = 50,
+    dendista = 60,
+    sogrinha = 450,
+];
+
+var total = aluguel + luz + agua + alimentação + rastreador + internet + academia + dendista + sogrinha;
+
+var saldo = salario - total;
+
+console.log('Seu salário é de: R$' + salario);
+console.log('Suas dispesas somaram um total de: R$' + total)
+
+if(total < salario){
+    console.log('Com base na sua receita e despesa, seu saldo atual é de: R$'+saldo+ ',ou seja, você está fazendo um bom uso do seu salário gastando menos do quê ganha.')
+};
+
+
+
  
