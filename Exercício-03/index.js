74617adea060bc9790b26bd6d851a4455dc75aff
Julio Costa <julio.github@hotmail.com>
2022-10-10 09:02:21 -0300

    Sistema de gastos familiar

--- Exercício-03/index.js
+++ Exercício-03/index.js
@@ -6,7 +6,7 @@
  // Agora, crie uma função que irá caucular o total de receitas e despesas e irá mostrar uma mensagem se a família está com saldo positivo ou negativo, seguido do valor do saldo.
 
  const receitas = [
-    salario = 800
+    salario = 2800
  ];
 
  const despesas = [
@@ -25,20 +25,19 @@ var total = aluguel + luz + agua + alimentação + rastreador + internet + acade
 
 var saldo = salario - total;
 
-console.log('Seu salário é de: R$ ' +salario.toLocaleString('pt-BR', {style:'currency', currency:'BRL'}));
-console.log('Suas dispesas somaram um total de: R$ ' + total.toLocaleString('pt-BR', {style:'currency', currency:'BRL'}))
+console.log('Seu salário é de:' +salario.toLocaleString('pt-BR', {style:'currency', currency:'BRL'}));
+console.log('Suas dispesas somaram um total de:' + total.toLocaleString('pt-BR', {style:'currency', currency:'BRL'}))
 
 if(total < salario){
-    console.log('Com base na sua receita e despesa, seu saldo atual é de: R$' +saldo+ 
-    ',ou seja, você está fazendo um bom uso do seu salário gastando menos do quê ganha.')
-
+    console.log('Com base na sua receita e despesa, seu saldo atual é de:' +saldo.toLocaleString('pt-BR', {style: 'currency', currency:'BRL'}) + ', ou seja, você está fazendo um bom uso do seu salário gastando menos do quê ganha.')
+    
 } else if(total == salario){
-    console.log('Com base na sua receita e despesas, seu saldo atual é de: R$' 
-    +saldo+ ',ou seja, você conseguiu pagar todas as suas contas, porém não sobrou nada.')
+    console.log('Com base na sua receita e despesas, seu saldo atual é de:' 
+    +saldo.toLocaleString('pt-BR', {style: 'currency', currency:'BRL'}) + ',ou seja, você conseguiu pagar todas as suas contas, porém não sobrou nada.')
     
 } else if(total > salario){
-    console.log('Com base na sua receita e despesas, seu saldo atual é de: R$'
-     +saldo+ ', ou seja, você ultrapassou seu saldo')
+    console.log('Com base na sua receita e despesas, seu saldo atual é de:'
+     +saldo.toLocaleString('pt-BR', {style: 'currency', currency:'BRL'})+ ', ou seja, você ultrapassou seu saldo')
 };
 
 
