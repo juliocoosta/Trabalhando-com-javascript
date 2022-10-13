d7b0efbb7327c8d91d64a78bcd6c407e6d90a13e
Julio Costa <julio.github@hotmail.com>
2022-10-13 10:31:30 -0300

    Finalizado

--- Exercícios/Buscando-e-encontrando-dados-em-Array/index.js
+++ Exercícios/Buscando-e-encontrando-dados-em-Array/index.js
@@ -9,7 +9,7 @@
     4- Transformar a função acima em uma funçao que irá receber o nome do autor e devolver os livros desse autor.
 */
 
-const categoriadelivros = [
+const bibliotecadelivros = [
 {
     categoria: 'Riqueza',
     livros:[
@@ -51,11 +51,60 @@ const categoriadelivros = [
 },  
 ];
 
-// 1- Contar o número de categoria e o número de livros em cada categoria:
-let totaldecategotia = categoriadelivros.length
-console.log(totaldecategotia);
+// 1- Contar o número de categoria.
+let totaldecategotia = bibliotecadelivros.length
+console.log('O total de categotias de livros:', totaldecategotia);
 
+//  e o número de livros em cada categoria:
+for(let categoria of bibliotecadelivros ){
+    console.log('Total de livros da categoria:', categoria.categoria)
+    console.log(categoria.livros.length)
+};
 
+// 2- Contar o número de autores:
+function contadordeautores(){
+    let autores = [];
 
+    for(let categoria of bibliotecadelivros) {
+        for(let livros of categoria.livros) {
+            if(autores.indexOf(livros.Autor) == -1) {
+                autores.push(livros.Autor)
+            }
+        }
+    }
+    console.log('O total de autores é:', autores.length)
+}
 
+    contadordeautores();
+
+// 3- Mostrar livros do autor Augusto Cury:
+function livrosdeaugustocury(){
+    let livros = [];
+
+    for(let categoria of bibliotecadelivros) {
+        for(let livro of categoria.livros) {
+            if(livro.Autor === 'Augusto Cury') {
+                livros.push(livro.Título)
+            }
+        }
+    }
+    console.log('O total de livros do autor Augusto Cury:', livros.length)
+}
+
+    livrosdeaugustocury();
+
+    // 4- Transformar a função acima em uma funçao que irá receber o nome do autor e devolver os livros desse autor.
+    function livrosdequalquerautor(autor){
+        let livros = [];
+    
+        for(let categoria of bibliotecadelivros) {
+            for(let livro of categoria.livros) {
+                if(livro.Autor === autor) {
+                    livros.push(livro.Título)
+                }
+            }
+        }
+        console.log(`Os livros do Autor, ${autor}: ${livros.join(', ')}`)
+    }
+    livrosdequalquerautor('Augusto Cury');
 
