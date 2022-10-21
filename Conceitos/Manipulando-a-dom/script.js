e6e6932a5ec3e2606c251300fddbb56289472ec6
Julio Costa <julio.github@hotmail.com>
2022-10-21 10:33:30 -0300

    Novos conceitos

--- Conceitos/Manipulando-a-dom/script.js
+++ Conceitos/Manipulando-a-dom/script.js
@@ -1,26 +1,27 @@
 // Usando o getElementById, dessa forma conseguimos usar qualquer elemento do html com ID.//
-const elemento = document.getElementById('Blog-tittle')
-console.log(elemento)
+
+/* const elemento = document.getElementById('Blog-tittle')
+console.log(elemento) */
 
 // Usando o getElementsByClassName, dessa forma conseguimos usar qualquer elemento do html com class.//
 
-const elemento1 = document.getElementsByClassName('Blog-tittle1')
-console.log(elemento1)
+/* const elemento1 = document.getElementsByClassName('Blog-tittle1')
+console.log(elemento1) */
 
 // Usando o getElementsByClassName, dessa forma conseguimos usar qualquer tag do html.//
 
-const elemento2 = document.getElementsByTagName('p')
-console.log(elemento2)
+/* const elemento2 = document.getElementsByTagName('p')
+console.log(elemento2) */
 
 // Usando o querySelector, dessa forma conseguimos usar qualquer tag do html sendo, mas ele pucha sempre o primeiro que ele encontrar.//
 
-const elemento3 = document.querySelector('p')
+/* const elemento3 = document.querySelector('p')
 console.log(elemento3)
-
+ */
 // Usando o querySelectorAll, dessa forma conseguimos usar todas as tags do html.//
 
-const elemento4 = document.querySelectorAll('p')
-console.log(elemento4)
+/* const elemento4 = document.querySelectorAll('p')
+console.log(elemento4) */
 
 /* 
 getElementById(elemento)
@@ -29,3 +30,36 @@ getElementsByTagName(html_coletion)
 querySelector(elemento)
 querySelectorAll(NodeListe)
 */
+
+//----------------------------------
+// Manipulando Conteúdos
+// TextContent
+
+/* const elemento = document.querySelector('h1')
+
+elemento.textContent  = 'lula 2022'
+
+console.log(elemento.textContent) */
+
+// innerText
+
+/* const elemento = document.querySelector('h1')
+
+elemento.innerText = 'Fala dev, blz?'
+
+console.log(elemento) */
+
+
+/* innerHTML */
+/* const elemento = document.querySelector('h1')
+
+elemento.innerHTML = 'Fala dev, blz? <h1>testando</h1>' 
+
+console.log(elemento) */  
+
+// value ou valor
+
+/* const elemento = document.querySelector('input')
+
+elemento.value = 'escreva alguma coisa' */
+
